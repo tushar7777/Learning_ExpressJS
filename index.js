eea2aa5b3b85562babd8f07ff412e0ae0a96fd49
tushar7777 <tusharchandra10@gmail.com>
2026-04-16 00:23:41 +0530

    added feat read file and display user data

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 8000;
+const filePath = './mock_data/MOCK_DATA.json';
 
 app.get('/', (req, res) => {
-    console.log('Hello World');
-    res.send('<h1>Hello World</h1>');
+    res.send('<h1>Home Page</h1>');
+})
+
+app.get('/api/users', (req, res) => {
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        res.send(JSON.parse(data));
+    });
+
 })
 
 app.listen(port, () => { console.log('Server is running on port ' + port) });
